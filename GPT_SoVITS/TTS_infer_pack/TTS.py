139282bc600877a201a5699f9f380ef2190c50f5
YYuX-1145 <138500330+YYuX-1145@users.noreply.github.com>
2025-03-27 07:57:29 +0800

    Add files via upload

--- GPT_SoVITS/TTS_infer_pack/TTS.py
+++ GPT_SoVITS/TTS_infer_pack/TTS.py
@@ -35,7 +35,7 @@ language=os.environ.get("language","Auto")
 language=sys.argv[-1] if sys.argv[-1] in scan_language_list() else language
 i18n = I18nAuto(language=language)
 
-
+from filelock import WindowsFileLock
 
 spec_min = -12
 spec_max = 2
@@ -301,20 +301,27 @@ class TTS_Config:
         else:
             print(i18n("路径不存在,使用默认配置"))
             self.save_configs(configs_path)
-        with open(configs_path, 'r', encoding='utf-8') as f:
-            configs = yaml.load(f, Loader=yaml.FullLoader)
-
+        lock=WindowsFileLock(f"{configs_path}.lock")
+        with lock:
+            with open(configs_path, 'r') as f:            
+                configs = yaml.load(f, Loader=yaml.FullLoader)    
         return configs
 
     def save_configs(self, configs_path:str=None)->None:
         configs=deepcopy(self.default_configs)
         if self.configs is not None:
             configs["custom"] = self.update_configs()
-
+            
         if configs_path is None:
             configs_path = self.configs_path
-        with open(configs_path, 'w') as f:
-            yaml.dump(configs, f)
+
+        lock=WindowsFileLock(f"{configs_path}.lock",timeout=3)
+        try:
+            with lock:
+                with open(configs_path, 'w') as f:
+                    yaml.dump(configs, f)
+        except:
+            print("failed to save config.")
 
     def update_configs(self):
         self.config = {
@@ -418,7 +425,7 @@ class TTS:
         if self.configs.is_half and str(self.configs.device)!="cpu":
             self.bert_model = self.bert_model.half()
 
-    def init_vits_weights(self, weights_path: str):
+    def init_vits_weights(self, weights_path: str, save=True):
         
         self.configs.vits_weights_path = weights_path
         version, model_version, if_lora_v3=get_sovits_version_from_path_fast(weights_path)
@@ -475,6 +482,8 @@ class TTS:
             self.configs.is_v3_synthesizer = True
             self.init_bigvgan()
             
+        if save:
+            self.configs.save_configs()            
 
         if if_lora_v3==False:
             print(f"Loading VITS weights from {weights_path}. {vits_model.load_state_dict(dict_s2['weight'], strict=False)}")
@@ -501,10 +510,11 @@ class TTS:
             self.vits_model = self.vits_model.half()
 
 
-    def init_t2s_weights(self, weights_path: str):
+    def init_t2s_weights(self, weights_path: str, save=True):
         print(f"Loading Text2Semantic weights from {weights_path}")
         self.configs.t2s_weights_path = weights_path
-        self.configs.save_configs()
+        if save:
+            self.configs.save_configs()
         self.configs.hz = 50
         dict_s1 = torch.load(weights_path, map_location=self.configs.device)
         config = dict_s1["config"]
